0455788f863376683dc080051b0c207d1a6db052
willjthomas <135294318+willjthomas@users.noreply.github.com>
2023-07-30 21:01:54 -0400

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,11 @@
 # add your code here
-
+for (i) in range(1, 101):
+    if i % 3 == 0:
+        if i % 5 == 0:
+            print("FIZZBUZZ")
+        else:
+            print("FIZZ")
+    elif i % 5 == 0:
+        print("BUZZ") 
+    else:
+        print(i)
